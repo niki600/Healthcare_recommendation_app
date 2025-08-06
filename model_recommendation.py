5ec8310ab40a0e6e8551f024c3b2513d47c6179d
niki600 <112570097+niki600@users.noreply.github.com>
2025-08-06 21:02:44 +0530

    Update model_recommendation.py

--- model_recommendation.py
+++ model_recommendation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pickle
 
-# Load model and scaler
+# Loading model and scaler
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
@@ -9,12 +9,23 @@ with open("scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
 
 def generate_recommendation(new_data):
+    # Convert to DataFrame
     new_df = pd.DataFrame([new_data])
-    new_scaled = scaler.transform(new_df.reindex(columns=scaler.feature_names_in_))
+
+    # Ensure same column order as training
+    new_df = new_df.reindex(columns=scaler.feature_names_in_)
+
+    # Scale the data
+    new_scaled = scaler.transform(new_df)
+
+    # Predict
     prediction = model.predict(new_scaled)[0]
+    
+     # 🔹 Flip output to match your report/PPT mapping
+    prediction = 1 - prediction
 
+    # Return result
     if prediction == 0:
-        return "✅ No immediate action needed. Maintain your current health routine."
+        return "No immediate action needed. Maintain your current health routine."
     else:
-        return "🚨 Please consult a doctor or take medical advice."
-
+        return "Please consult a doctor or take medical advice."
